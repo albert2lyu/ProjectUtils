a2fa9600ae4cff5742125fccf3872241eb74e7d8
autod_android_dev_spencer <sunhaozhen@news18a.com>
2015-07-20 14:33:31 +0800

    update SharedPreferencesUtils

--- app/src/main/java/com/sunhz/projectutils/fileutils/SharePreferenceUtils.java
+++ app/src/main/java/com/sunhz/projectutils/fileutils/SharePreferenceUtils.java
@@ -20,81 +20,78 @@ public class SharePreferenceUtils {
     }
 
     public synchronized void saveLong(String spName, String key, long content) {
-        mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).edit().putLong(key, content)
+        mContext.getSharedPreferences(spName, Context.MODE_PRIVATE | Context.MODE_MULTI_PROCESS).edit().putLong(key, content)
                 .commit();
     }
 
-    public synchronized void saveLongAppend(String spName, String key, long content) {
-        mContext.getSharedPreferences(spName, Context.MODE_APPEND).edit().putLong(key, content)
-                .commit();
-    }
 
     public synchronized long getLong(String spName, String key) {
-        return mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).getLong(key, -1);
+        return mContext.getSharedPreferences(spName, Context.MODE_PRIVATE | Context.MODE_MULTI_PROCESS).getLong(key, -1);
     }
+
+    public synchronized long getLong(String spName, String key, long defaultValue) {
+        return mContext.getSharedPreferences(spName, Context.MODE_PRIVATE | Context.MODE_MULTI_PROCESS).getLong(key, defaultValue);
+    }
+
 
     public synchronized void saveInt(String spName, String key, int content) {
-        mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).edit().putInt(key, content)
-                .commit();
-    }
-
-    public synchronized void saveIntAppend(String spName, String key, int content) {
-        mContext.getSharedPreferences(spName, Context.MODE_APPEND).edit().putInt(key, content)
+        mContext.getSharedPreferences(spName, Context.MODE_PRIVATE | Context.MODE_MULTI_PROCESS).edit().putInt(key, content)
                 .commit();
     }
 
     public synchronized int getInt(String spName, String key) {
-        return mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).getInt(key, -1);
+        return mContext.getSharedPreferences(spName, Context.MODE_PRIVATE | Context.MODE_MULTI_PROCESS).getInt(key, -1);
+    }
+
+    public synchronized int getInt(String spName, String key, int defaultValue) {
+        return mContext.getSharedPreferences(spName, Context.MODE_PRIVATE | Context.MODE_MULTI_PROCESS).getInt(key, defaultValue);
     }
 
     public synchronized void saveBoolean(String spName, String key, boolean content) {
-        mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).edit().putBoolean(key, content)
-                .commit();
-    }
-
-    public synchronized void saveBooleanAppend(String spName, String key, boolean content) {
-        mContext.getSharedPreferences(spName, Context.MODE_APPEND).edit().putBoolean(key, content)
+        mContext.getSharedPreferences(spName, Context.MODE_PRIVATE | Context.MODE_MULTI_PROCESS).edit().putBoolean(key, content)
                 .commit();
     }
 
     public synchronized boolean getBoolean(String spName, String key) {
-        return mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).getBoolean(key, false);
+        return mContext.getSharedPreferences(spName, Context.MODE_PRIVATE | Context.MODE_MULTI_PROCESS).getBoolean(key, false);
+    }
+
+    public synchronized boolean getBoolean(String spName, String key, boolean defaultValue) {
+        return mContext.getSharedPreferences(spName, Context.MODE_PRIVATE | Context.MODE_MULTI_PROCESS).getBoolean(key, defaultValue);
     }
 
     public synchronized void saveString(String spName, String key, String content) {
-        mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).edit().putString(key, content)
-                .commit();
-    }
-
-    public synchronized void saveStringAppend(String spName, String key, String content) {
-        mContext.getSharedPreferences(spName, Context.MODE_APPEND).edit().putString(key, content)
+        mContext.getSharedPreferences(spName, Context.MODE_PRIVATE | Context.MODE_MULTI_PROCESS).edit().putString(key, content)
                 .commit();
     }
 
     public synchronized String getString(String spName, String key) {
-        return mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).getString(key, "");
+        return mContext.getSharedPreferences(spName, Context.MODE_PRIVATE | Context.MODE_MULTI_PROCESS).getString(key, "");
+    }
+
+    public synchronized String getString(String spName, String key, String defaultValue) {
+        return mContext.getSharedPreferences(spName, Context.MODE_PRIVATE | Context.MODE_MULTI_PROCESS).getString(key, defaultValue);
     }
 
     public synchronized void saveFloat(String spName, String key, float content) {
-        mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).edit().putFloat(key, content)
-                .commit();
-    }
-
-    public synchronized void saveFloatAppend(String spName, String key, float content) {
-        mContext.getSharedPreferences(spName, Context.MODE_APPEND).edit().putFloat(key, content)
+        mContext.getSharedPreferences(spName, Context.MODE_PRIVATE | Context.MODE_MULTI_PROCESS).edit().putFloat(key, content)
                 .commit();
     }
 
     public synchronized float getFloat(String spName, String key) {
-        return mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).getFloat(key, -1);
+        return mContext.getSharedPreferences(spName, Context.MODE_PRIVATE | Context.MODE_MULTI_PROCESS).getFloat(key, -1);
+    }
+
+    public synchronized float getFloat(String spName, String key, float defaultValue) {
+        return mContext.getSharedPreferences(spName, Context.MODE_PRIVATE | Context.MODE_MULTI_PROCESS).getFloat(key, defaultValue);
     }
 
     public synchronized boolean contains(String spName, String key) {
-        return mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).contains(key);
+        return mContext.getSharedPreferences(spName, Context.MODE_PRIVATE | Context.MODE_MULTI_PROCESS).contains(key);
     }
 
     public synchronized void clear(String spName) {
-        mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).edit().clear().commit();
+        mContext.getSharedPreferences(spName, Context.MODE_PRIVATE | Context.MODE_MULTI_PROCESS).edit().clear().commit();
     }
 
 }
